568b157887151018c5247dac16f17d15ae18aa6c
Arshiya <Arshiya@Arshiya-VAIO>
2018-06-03 08:54:53 +0530

    Commit Test

---
+++ src/test/java/Maven_GID/Maven_AID/Testng_Test.java
@@ -0,0 +1,16 @@
+package Maven_GID.Maven_AID;
+
+import org.testng.annotations.Test;
+
+public class Testng_Test {
+@Test()
+public void Testng_Message()
+{
+	System.out.println("From TestNG");
+}
+@Test()
+public void Testng_Message1()
+{
+	System.out.println("From TestNG");
+}
+}
